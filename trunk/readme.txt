78d63090a9290d9a56af3d8e9ba159f79a96584e
aaronfessler <aaronfessler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-24 01:53:48 +0000

    tagging version 2.5.1
    git-svn-id: https://plugins.svn.wordpress.org/twine-social/tags/2.5.1@953844 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,176 @@
+=== Twine Social: Show your social media content in a easy and beautiful interface ===
+Contributors: Nathan Elliott, Aaron Fessler
+Tags: Twine Social, facebook widget, twitter widget, instagram widget, hashtag campaign manager, hashtags, hashtag campaigns, linkedin widget, social media feed, Social Media Hub, plugin, widget, social networks, twitter, facebook, social media, Pinterest Widget, publish Facebook, social plugins, social media aggregator, rss widget, embed social media feeds, display social media, youtube widget, twitter posts, twitter sidebar, Facebook posts, Facebook sidebar, social sidebar, plus one, google widget, share this, sharethis, socialize, addthis, autoblogger, autoblog, feed aggregator
+Requires at least: 3.0
+Tested up to: 3.9.1
+Stable tag: 2.5.1
+License: GPLv2
+
+Display your social media content (Facebook, Twitter, Instagram, YouTube, Vine, Pinterest + more) on your Wordpress blog in one beautiful interface.
+
+== Description ==
+
+[Twine Social](http://www.twinesocial.com) provides the tools you need to grow and engage your social audience. Display any social media content on your Wordpress site, and drive massive social engagement.
+
+This plugin makes it super easy to add your social media content (including #hashtags, @accounts, videos, photos, and tweets) from any social media network to your Wordpress Blog. [Twine Social](http://www.twinesocial.com) is richly interactive and engaging. Get beautiful presentation of your photos, videos, and other social media elements on your Wordpress site.
+
+Your customers see your brand in action, making Twine the perfect social media hub.
+
+= Features =
+
+* A beautiful view of your social media content from all major networks: Facebook, Twitter, Instagram, YouTube, Vine, Flickr, Pinterest, Tumblr, Linkedin, and RSS.
+* Gather your content by @username, #hashtag, location, and more.
+* Setup filters to automatically eliminate unwanted content and retweets.
+* Video player for content with cross-browser and full-screen playback support.
+* URL expander takes embedded and shortened URLs that appear in your social media stream, automatically extracting titles, headlines, and key photos
+* Moderate your social media content right from your Wordpress blog with one click.
+* Group your content by category (Press Reviews, Blog, News, Products, Events), and embed slices of your social media stream on individual Wordpress pages.
+* Permalinks to each of your social media tiles, optimized for SEO.
+* Responsive grid for dynamic layouts.
+* Infinite scroll: load more content simply by scrolling to the bottom of the page.
+* Auto-approve all your user generated content, or set it up for prior moderation.
+* Custom CSS: Style it to fit your brand.
+* Klout: Get an automatic weekly email each time an influential follower follows your Twitter account.
+* Insights: Get an automatic weekly email showing your likes and follower performance over time.
+
+**IMPORTANT: You will FIRST need to go to [Twine Social](http://www.twinesocial.com), sign up for a FREE account before you can embed it into your Wordpress Site. ** 
+
+If you have any questions or feedback, please visit our [support forum](http://twinesocial.uservoice.com/), or email us at support{at}twinesocial.com.
+
+== Installation ==
+
+First, go to [Twine Social](http://www.twinesocial.com) and sign up for a FREE account. Installing the plugin without a [Twine Social](http://www.twinesocial.com) account will not work properly.
+
+Twine Social can be displayed on the Wordpress sidebar or embedded in your page as a shortcode. It works best when featured on a full page.
+
+
+= Instructions for placing your Twine Social feed on the Sidebar of your Wordpress site =
+
+1. Download the plugin and upload it into your Wordpress Plugins folder OR search "Twine Social" in the plugin store and install it.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Click on Settings -> Twine Social Settings.
+4. You'll need to add your Twine Social Account ID. You can find it by logging in to [Twine Social Admin](http://customer.twinesocial.com), using the account you previously created. Click on settings in the top right hand corner and your Account ID will be displayed. It's a short code, like 13-ICZ5I4.
+5. You may have more than one Twine Social application - one for each brand you manage. Choose one.
+6. You can publish your Twine Social application to your Wordpress Home Page, or to create a new Wordpress Page.
+
+That's it!
+
+= Instructions for placing your Twine feed in a Wordpress template sidebar =
+
+1. Go to the Appearance -> Widgets, drag the "Twine Social" widget to your "Main Sidebar"
+2. Choose from your list of available Twine Social applications.
+3. Setup Height and number of columns. (Leave height blank for 100%).
+
+= Instructions for placing your Twine feed directly in a post or page using shortcode =
+
+1. Create a new Post and click on the HTML or Text option on the top-right hand side of the post. 
+2. Use the shortcode twine: Example [twinesocial app='SITE' cols=2] (Note: Replace SITE with your application name.)
+3. Before you publish, click Visibility- Public 
+
+Arguments supported by the shortcode:
+
+* 'app' Your Twine Social application id
+* 'width' width of the iframe
+* 'height' height of the iframe
+* 'cols' number of columns to show in the view (default: 4)
+* ‘topic’ the specific topic to filter, like “Videos” or “Press Releases”
+* 'scroll=auto' for infinite scrolling
+
+Shortcode example:
+[twinesocial site_name=“WCD” cols=4 topic=“Videos”]
+
+Many column layouts are supported:
+1 = 1 column wide
+2 = 2 columns, each 50% wide
+3 = 3 columns, each 33% wide
+363 = 3 columns, 25%, 50%, and 25% wide.
+4 = 4 columns, each 25% wide (default)
+
+== Frequently Asked Questions == 
+ 
+= What is Twine? =
+
+[Twine Social](http://www.twinesocial.com) provides the tools you need to grow and engage your social audience. Display all of your brand’s social media content on your digital properties, and drive massive social engagement.
+
+= What can I use Twine Social for? =
+
+Connect any combination of social media feeds, including hashtags, from any social media network and [Twine Social](http://www.twinesocial.com) will aggregate your content onto one stunning destination on your website, TV, video wall, or display.
+
+[Twine Social](http://www.twinesocial.com) is richly interactive and engaging. Get beautiful presentation of your photos, videos, and other social media elements on your digital properties.
+
+
+= How do I start using Twine Social? =
+
+First sign up for a free account at [Twine Social](http://www.twinesocial.com). This Wordpress plugin then allows you to embed your Twine Social feed into your WordPress site. Go to the Installation tab to easily learn how.
+
+
+= What does Twine cost? =
+
+It’s totally free! When you install this plugin and get your own free Twine Social account, we’ll give you a full-access account for 30 days. If you choose not to buy a paid plan (unlocking additional sweet features), your account will automatically convert to a free account. No credit card required.
+
+= Help, I'm having a problem! =
+
+We apologize for any problems you may have. Feel free to visit our [support forums](http://twinesocial.freshdesk.com), send us a tweet at @twinesocial, or email us at support{at}twinesocial.com
+
+== Screenshots ==
+
+1. Embed a social media feed wall of your social media content on your Wordpress page. Twine Social supports responsive layouts, integrated video player, and infinite scroll.
+2. Responsive layout and infinite scroll. Choose your layout and theme.
+3. An intuitive settings panel helps you configure your Plugin.
+4. Embed your Twine Social feed in a widget/sidebar
+5. Configure your Twine Social widget.
+
+
+== Changelog ==
+
+= 2.5.1 =
+BIGGEST UPDATE YET: LOADED WITH NEW FEATURES
+Significant enhancements to widget administration. Now see the status of your social connections.
+Support for embedding layouts and collections
+Manage your Social Engine Rules
+
+
+= 2.3 =
+Removed dependency on cURL library.
+Tested with WordPress 3.9.1
+
+= 2.0 = 
+A snazzy new Featured Video carousel showcases the most popular videos in your social hub.
+Now featuring Topic Navigation.
+Support for infinite scroll in the sidebar Widget
+Improved documentation.
+Tested with Wordpress 3.8.1.
+
+= 1.6 = 
+Added Featured Video carousel. Improved detail view of posts. Tested with Wordpress 3.8.1
+
+= 1.4.2 = 
+Improved layout. Support for Wordpress 3.8
+
+= 1.4.1 = 
+Improved layout of YouTube and Vimeo elements
+
+= 1.4 =
+* Now with even more beautiful layouts for responsive, mobile, and table displays.
+
+= 1.3.1 =
+* Added new support multiple Twine Social applications. Choose your specific application when setting up the Wordpress Widget.
+
+= 1.3 =
+* Added new support for Topics in short codes and Widgets. Now, group and display social your content, like "Videos" or "Press Releases" in relevant sections of your Wordpress blog. Super awesome! Setup your Topics in your [Twine Social Admin Console](http://customer.twinesocial.com/appTopic/edit).
+
+= 1.2 =
+* Improved support for transparent IFRAMES
+
+= 1.1 =
+* Upgraded support libraries to Bootstrap 3.
+
+= 1.0 =
+* Initial Version
+
+== Upgrade Notice ==
+No problems should be encountered when upgrading.
+
+
+
+
---
+++ trunk/twinesocial-widget.php
@@ -0,0 +1,403 @@
+<?php
+/*
+ * Plugin Name: Twine Social Widget
+ * Plugin URI: http://www.twinesocial.com
+ * Description: Display your social media content with the Twine Social Wordpress plugin - including hashtags and user content - in a beautiful and richly interactive view.
+ * Version: 2.5.1
+ * Author: Nathan Elliott
+ * License: GPLv2 or later
+ */
+
+require_once dirname( __FILE__ ) . '/twinesocial-admin.php';
+
+if (!defined("TWINE_PLUGIN_DIRNAME"))   define("TWINE_PLUGIN_DIRNAME",  plugin_basename(dirname(__FILE__)) );
+if (!defined("TWINE_PUBLIC_URL"))   define("TWINE_PUBLIC_URL",  "//www.twinesocial.com");
+if (!defined("TWINE_APPS_URL"))   define("TWINE_APPS_URL",  "//apps.twinesocial.com");
+if (!defined("TWINE_CUSTOMER_URL"))   define("TWINE_CUSTOMER_URL",  "//customer.twinesocial.com");
+
+
+/**
+ * Register the Widget
+ */
+add_action( 'widgets_init', create_function( '', 'register_widget("twinesocial_widget");' ) );
+
+/**
+ * Create the widget class and extend from the WP_Widget
+ */
+ class twinesocial_Widget extends WP_Widget {
+
+	/**
+	 * Set the widget defaults
+	 */
+	private $widget_title = "Twine Social";
+	private $app = "";
+	private $height = "";
+	private $width = "";
+	private $scroll = "";
+	private $collection = "";
+	private $carousel = "1";
+	private $nav = "0";
+	private $noAnimate = false;
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+
+		parent::__construct(
+			'twinesocial_Widget', // Base ID
+			'Twine Social ',      // Name
+			array(
+				'classname'		=>	'twinesocial_Widget',
+				'description'	=>	__('A WordPress widget to add your TwineSocial Hub.', 'framework')
+			)
+		);
+	} // end constructor
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	 
+	 // this only affects the sidebar widget
+	public function widget( $args, $instance ) {
+		extract( $args );
+
+		/* Our variables from the widget settings. */
+		$this->widget_title = apply_filters('widget_title', $instance['title'] );
+
+
+		$this->height     = $instance['height'];
+		$this->app        = $instance['app'];
+		$this->collection = isset($instance['collection']) ? $instance['collection'] : null;
+		$this->carousel   = isset($instance['carousel']) ? $instance['carousel'] : null;
+		$this->scroll     = $instance['scroll'];
+		$this->noAnimate  = true ;
+
+		/* Before widget (defined by themes). */
+		echo $before_widget;
+
+		/* Display the widget title if one was input (before and after defined by themes). */
+		if ( $this->widget_title )
+			echo $before_title . $this->widget_title . $after_title;
+
+        $this->render( array('app' => $this->app
+                            , 'collection' => $this->collection
+                            , 'height' => $this->height
+                            , 'carousel' => $this->carousel
+                            , 'noAnimate' => $this->noAnimate
+                            , 'scroll' => $this->scroll
+                            , 'nav' => $this->nav
+                            ) );
+
+		/* After widget (defined by themes). */
+		echo $after_widget;
+	}
+
+    public function render( $args ) {
+        echo $this->tw_render( $args );
+    }
+
+    public function tw_render( $args ) {
+        $r = wp_parse_args( $args, array('app' => ''
+                                    , 'height' => ''
+                                    , 'width' => ''
+                                    , 'collection' => ''
+                                    , 'scroll' => ''
+                                    , 'carousel' => ''
+                                    , 'noAnimate'=>false
+                                    , 'nav' => ''
+                                    ) );
+
+        $r['app'] = $this->clean_appname( $r['app'] );
+
+        $url = add_query_arg( array(
+            'app'      => rawurlencode( $r['app'] ),
+        ), TWINE_APPS_URL . '/embed' );
+
+
+        if (!empty( $r['height'])) {
+            $url = add_query_arg( 'height', $r['height'], $url );
+        }
+
+        if (strlen($r['scroll'])==0) {
+            $url = add_query_arg( 'scroll', "no", $url );
+        }
+
+        if ( !empty( $r['collection'] ) )
+            $url = add_query_arg( 'collection', $r['collection'], $url );
+
+        if ($r['carousel']=="1")
+            $url = add_query_arg( 'showCarousel', "1", $url );
+
+        if ($r['nav']=="1")
+            $url = add_query_arg( 'showNav', "1", $url );
+
+		if ($r['noAnimate']) {
+	        $url = add_query_arg( 'noAnimate', "1", $url );
+	    }
+
+		$code = substr(md5(uniqid(mt_rand(), true)) , 0, 8);
+
+        $output = '<script type="text/javascript" id="twine-script" data-instance-id="' . $code . '" src="' . esc_url( $url ) . '"></script>';
+
+        return $output;
+    }
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		/* Strip tags for title and name to remove HTML (important for text inputs). */
+		$instance['app'] = strip_tags( $new_instance['app'] );
+
+		$instance['height'] = strip_tags( $new_instance['height'] );
+		$instance['collection'] = strip_tags( $new_instance['collection'] );
+		$instance['carousel'] = strip_tags( $new_instance['carousel'] );
+		$instance['nav'] = strip_tags( $new_instance['nav'] );
+		$instance['scroll'] = strip_tags( $new_instance['scroll'] );
+
+		return $instance;
+	}
+
+    function clean_appname($appname) {
+        if (! empty($appname))
+            return preg_replace('/^http(s)?:\/\/(www.)?twinesocial.com\//', '', $appname);
+        else
+            '';
+    }
+
+	/**
+	 * Create the form for the Widget admin
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	function form( $instance ) {
+
+
+		$twinesocial_appdata = get_option('twinesocial_appdata');
+
+	    wp_enqueue_script('twinesocial_widget_js3', plugins_url('/js/twine.js', __file__ ) );
+
+		/* Set up some default widget settings. */
+		$defaults = array(
+                'title' => $this->widget_title,
+                'app' => $this->app,
+                'height' => $this->height,
+                'collection' => $this->collection,
+                'carousel' => $this->carousel,
+                'scroll' => $this->scroll,
+                'nav' => $this->nav,
+                'width' => $this->width,
+            );
+
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+		<div class="tw-widget-content">
+
+            <!-- Page name: Text Input -->
+
+             <H4>Configure your Twine Social Sidebar Widget</H4>
+
+            <p>
+                <label for="<?php echo $this->get_field_id( 'app' ); ?>"><?php _e('TwineSocial Hub:', 'framework') ?>: </label><BR>
+
+
+				<?php 
+				if ($twinesocial_appdata) {
+					$js = json_decode($twinesocial_appdata);
+					if ($js->success) { ?>
+						<SELECT id="twinesocial_baseUrl" name="<?php echo $this->get_field_name( 'app' ); ?>" >
+						<?php foreach ($js->apps as $app) {
+
+							$sel = $instance['app']==$app->baseUrl ? 'selected' : '';
+							echo '<OPTION ' . $sel . ' value="' . $app->baseUrl . '">' . $app->name . '</option>';
+						}
+						echo '</SELECT>';
+					} else {
+						echo '<div class="alert alert-info">' . $js->message . '</div>';
+					}
+			} ?>
+						
+            </p>
+
+
+            <p>
+                <label for="<?php echo $this->get_field_id( 'collection' ); ?>"><?php _e('Collection', 'framework') ?>: </label><BR>
+
+				<?php 
+				if ($twinesocial_appdata) {
+					$js = json_decode($twinesocial_appdata); ?>
+					<SELECT id="twinesocial_collection" name="<?php echo $this->get_field_name( 'collection' ); ?>">
+						<OPTION value="0">Show All Items</option>
+						<?php foreach ($js->apps as $app) {
+							if ($app->baseUrl == $js->apps[0]->baseUrl) {
+								foreach ($app->collections as $collection) { 
+									$sel = $instance['collection']==$collection->id ? 'selected="selected"' : '';
+									echo '<OPTION ' . $sel . ' value="' . $collection->id . '">' . $collection->name . '</option>';
+								}
+							}
+						}
+					echo '</SELECT>';
+			} ?>
+						
+            </p>
+
+
+            <!-- Widget Title: Text Input -->
+            <p>
+                <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Widget Title', 'framework') ?>: </label>
+                <input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" />
+            </p>
+
+			<hr>
+            <p>
+            <strong>Advanced Settings:</strong>
+            </p>
+
+            <p>
+                <label for="<?php echo $this->get_field_id( 'height' ); ?>"><?php _e('Height', 'framework') ?>: </label>
+                <input type="text" id="<?php echo $this->get_field_id( 'height' ); ?>" name="<?php echo $this->get_field_name( 'height' ); ?>" value="<?php echo $instance['height']; ?>" size="5" />px
+                <br> <small>Initial height of the sidebar widget.</small>
+            </p>
+
+            <p>
+                <input type="checkbox" value="yes" id="<?php echo $this->get_field_id( 'scroll' ); ?>" name="<?php echo $this->get_field_name( 'scroll' ); ?>"  <?php echo $instance['scroll'] ? "checked='checked'" : "" ?> />
+                <label for="<?php echo $this->get_field_id( 'scroll' ); ?>">Infinite Scrolling</label>
+
+                <br> <small>Check if you want the widget to automatically grow when the bottom of the page is reached.</small>
+            </p>
+
+        </div>
+
+	<?php
+	}
+ }
+
+
+/**
+ * Display the embed of twinesocial stream
+ *
+ * The list of arguments is below:
+ *     'app' (string) - You twinesocial site id
+ *     'height' (int) - height of the iframe
+ *
+ * @param string|array $args Optional. Override the defaults.
+ */ 
+function twinesocial_embed( $args ) {
+    $tw_widget = new twinesocial_Widget();
+    $tw_widget->tw_render( $args );
+}
+
+/**
+ * Shortcode to diplay Twine Social in your site.
+ * 
+ * The list of arguments is below:
+ *     'app' (string) - You twinesocial site id
+ *                    Default: twinesocial
+ *     'height' (int) - height of the iframe
+ *                    Default: initial auto adjustable to 20 posts.
+ * 
+ * Usage: 
+ * [twinesocial app="twinesocial"]
+ * [twinesocial app="twinesocial" height="1500"]
+ */
+function twinesocial_shortcode( $atts ) {
+    $tw_widget = new twinesocial_Widget();
+
+    $height = '';
+    if ( isset($atts['height']) && !empty( $atts['height'] ) ) {
+        $height = $atts['height'];
+    }
+
+    $width = '0';
+    if ( isset($atts['width']) && !empty( $atts['width'] ) ) {
+        $width = $atts['width'];
+    }
+
+    $app = '';
+    if (isset($atts['app']) && ! empty( $atts['app'] ) ) {
+        $app = $atts['app'];
+    }
+
+    $collection = '';
+    if (isset($atts['collection']) && ! empty( $atts['collection'] ) ) {
+        $collection = urlencode($atts['collection']);
+    }
+
+    $scroll = '';
+    if (empty($atts['scroll'])) {
+        $scroll = "no";
+    }
+
+    $carousel = '0';
+    if (isset($atts['carousel']) && ! empty( $atts['carousel'] ) ) {
+        $carousel = $atts['carousel'];
+    }
+
+    $nav = '0';
+    if (isset($atts['nav']) && !empty( $atts['nav'] ) ) {
+        $nav = '1';
+    }
+
+    return $tw_widget->tw_render( array(
+        'app'     => $app,
+        'height'        => $height,
+        'scroll'        => $scroll,
+        'collection'        => $collection,
+        'width'        => $width,
+        'nav'        => $nav,
+        'noAnimate'        => false,
+        'carousel'        => $carousel
+    ) );
+}
+add_shortcode( 'twinesocial', 'twinesocial_shortcode' );
+
+/**
+* This function is in charge of sending the "get started" email
+*/
+function send_welcome_email() {	
+	$to = get_bloginfo('admin_email');
+	$subject = 'Build Your Social Hub';
+	$message = "
+	<p>Hi there,</p>
+	<p>Thank you for trying TwineSocial for WordPress. I’m Samuel Barnett, here to help you create your perfect social media hub with Twine.</p>
+	<p>To start, please sign up for a <a href='http://www.twinesocial.com/?utm_source=wp-welcome'>TwineSocial account</a>. It’s totally free and <a href='http://www.twinesocial.com/?utm_source=wp-welcome'>only takes a few minutes</a>. Then, connect up your networks and hashtags.</p>
+	<p>Finally, enter your account ID (see the upper righthand corner of your admin) into your WordPress plugin. And boom! You’re done!</p>
+	<p>Questions? Hit “reply” and let me know how I can help. It's what I'm here for.</p>
+	<br />
+	Samuel Weaver<br />
+	Customer Support<br />
+	<a href='mailto:support@twinesocial'>support@twinesocial.com</a><br /><br />
+	<img width='206' height='30' src='http://static.twinesocial.com/images/wp-logo.png' alt='TwineSocial' title='TwineSocial'>
+	";
+	$headers = "From: TwineSocial <support@twinesocial.com>\r\n";
+	$headers.= "Reply-To: Samuel Weaver <support@twinesocial.com>\r\n";
+	$headers.= "X-Mailer: PHP/" . phpversion() . "\r\n";
+	$headers.= "MIME-Version: 1.0\r\n";
+	$headers.= "Content-type: text/html; charset=utf-8\r\n";
+
+	if (function_exists('wp_mail')){
+		wp_mail($to, $subject, $message, $headers);
+	}
+}
+
+// Sets welcome email to send after activation.
+register_activation_hook( __FILE__, 'send_welcome_email' );
+
+
+?>
